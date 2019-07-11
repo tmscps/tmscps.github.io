80988be17bb35d997d1e01508c192d09730eea5d
tmscps <52742941+tmscps@users.noreply.github.com>
2019-07-12 00:27:58 +1000

    sd

--- video.js
+++ video.js
@@ -155,7 +155,7 @@ function scanBarcode() {
     setTimeout(scanBarcode, 30);
   }
 }
-// https://github.com/samdutton/simpl/tree/gh-pages/getusermedia/sources 
+// https://github.com/samdutton/simpl/tree/gh-pages/getusermedia/sources
 var videoSelect = document.querySelector('select#videoSource');
 
 navigator.mediaDevices.enumerateDevices()
@@ -203,4 +203,11 @@ function gotStream(stream) {
 
 function handleError(error) {
   console.log('Error: ', error);
+}
+if (barcode_result == '5035226121623') {
+  document.getElementById("title").innerHTML = "FENDER 2-BUTTON FOOTSWITCH CHANNEL"
+  document.getElementById("sdk").innerHTML = "0071359000"
+  document.getElementById("cost").innerHTML = "$55.00"
+  document.getElementById("imgbar").src="imgs/Fender-2-Button-Footswitch-Pedal-768x768.jpg";
+
 }

0f9b66617056a8051c0de00ec353d5540e5a7665
tmscps <52742941+tmscps@users.noreply.github.com>
2019-07-12 01:14:25 +1000

    wf

--- products.js
+++ products.js
@@ -1,7 +1,13 @@
-if (document.getElementById('dbr').innerHTML == "5035226121623" ) {
+function TextChange(tBox) {
+  if(event.propertyName=='5035226121623') {
+
+// if (document.getElementById('dbr').innerHTML == "5035226121623" ) {
   document.getElementById("title").innerHTML = "FENDER 2-BUTTON FOOTSWITCH CHANNEL"
   document.getElementById("sdk").innerHTML = "0071359000"
   document.getElementById("cost").innerHTML = "$55.00"
   document.getElementById("imgbar").src="imgs/Fender-2-Button-Footswitch-Pedal-768x768.jpg";
 
 }
+
+        }
+    

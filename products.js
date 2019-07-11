03de728ca85ce42a9a088feb824f48e8ccf68bef
tmscps <52742941+tmscps@users.noreply.github.com>
2019-07-11 23:10:32 +1000

    sd

---
+++ products.js
@@ -0,0 +1,7 @@
+if (barcode_result = '5035226121623') {
+  document.getElementById("title").innerHTML = "FENDER 2-BUTTON FOOTSWITCH CHANNEL"
+  document.getElementById("sdk").innerHTML = "0071359000"
+  document.getElementById("cost").innerHTML = "$55.00"
+  document.getElementById("imgbar").src="imgs/Fender-2-Button-Footswitch-Pedal-768x768.jpg";
+
+}

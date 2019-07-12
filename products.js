9e8bfd1851ded3ee8be0ae720b2b7ef259edbe20
tmscps <52742941+tmscps@users.noreply.github.com>
2019-07-12 20:06:58 +1000

    da

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
-
+$(function(){
 if ($("#dbr").text() == "5035226121623")
 {
   document.getElementById("title").innerHTML = "FENDER 2-BUTTON FOOTSWITCH CHANNEL";
@@ -7,3 +7,4 @@ if ($("#dbr").text() == "5035226121623")
   document.getElementById("imgbar").src="imgs/Fender-2-Button-Footswitch-Pedal-768x768.jpg";
 
 }
+})

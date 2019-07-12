378a24ea1c2434ad2da29cb0b615e1ad1438f5b4
tmscps <52742941+tmscps@users.noreply.github.com>
2019-07-12 14:17:09 +1000

    ge

--- products.js
+++ products.js
@@ -1,4 +1,6 @@
-if (decodePtr == '5035226121623') {
+
+if ($("#dbr").text() == "5035226121623")
+{
   document.getElementById("title").innerHTML = "FENDER 2-BUTTON FOOTSWITCH CHANNEL";
   document.getElementById("sdk").innerHTML = "0071359000";
   document.getElementById("cost").innerHTML = "$55.00";
